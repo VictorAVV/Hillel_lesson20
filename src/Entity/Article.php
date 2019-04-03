1cf8774d163aa93365f873431014369fc8095492
VictorAVV <46534051+VictorAVV@users.noreply.github.com>
2019-04-04 00:05:51 +0300

    add validation field of new Articles and new Category

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
@@ -18,11 +19,19 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Length(
+     *      min = 3,
+     *      max = 250,
+     *      minMessage = "Заголовок статьи должен быть длиннее {{ limit }} символов",
+     *      maxMessage = "Заголовок статьи должен быть короче {{ limit }} символов"
+     * )
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\Length(min = 5)
      */
     private $content;
 
@@ -33,6 +42,10 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=100, nullable=true)
+     * @Assert\Length(
+     *      max = 100,
+     *      maxMessage = "Заголовок статьи должен быть менее {{ limit }} символов"
+     * )
      */
     private $author;
 
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -14,9 +14,9 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class, ['label' => 'Название статьи:'])
-            ->add('content', null, ['label' => 'Текст статьи:'])
-            ->add('author', TextType::class, ['label' => 'Автор:'])
+            ->add('title', TextType::class, ['label' => 'Название статьи: *'])
+            ->add('content', null, ['label' => 'Текст статьи: *', 'attr' => array('rows' => '5')])
+            ->add('author', TextType::class, ['label' => 'Автор:', 'required' => false])
             ->add('category', null, ['label' => 'Категория:'])
         ;
     }
